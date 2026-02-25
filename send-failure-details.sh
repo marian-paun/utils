6d5db2585b7fe62cb706c3ce1691350f6daf960a
marp <marian.paun@gmail.com>
2026-02-25 11:48:00 +0200

    20260225 - added send-failure-details

---
+++ send-failure-details.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+UNIT=$1
+HOSTNAME=$(hostname)
+
+# 1. Get the status (ActiveState, Result, etc.)
+STATUS=$(systemctl show "$UNIT" --property=ActiveState,Result,FileDescriptorStoreMax --value | tr '\n' ' ')
+
+# 2. Grab the last 5 lines of logs for context
+LOGS=$(journalctl -u "$UNIT" -n 5 --no-hostname --no-pager)
+
+# 3. Format the message
+MESSAGE="🚨 *Service Failure Alert* 🚨
+*Host:* $HOSTNAME
+*Service:* $UNIT
+*State:* $STATUS
+*Last Logs:*
+$LOGS"
+
+/usr/local/bin/pushover -p 2 -s echo -Title "Service Failure on $HOSTNAME" "$MESSAGE"
